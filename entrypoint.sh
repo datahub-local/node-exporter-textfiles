6340dd437136a29c41b4c43db79e4915ae225b3d
Alvaro Santos Andres <alvsanand@gmail.com>
2025-06-01 08:32:40 +0200

    feat: can execute multiple SCRIPTS at a time

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,21 +4,29 @@ if [ -n "$DEBUG" ]; then
     set -ex
 fi
 
-SCRIPTS_PATH="${SCRIPTS_PATH:-/scripts}"
+if [ -z "$SCRIPTS" ] && [ -n "$SCRIPT" ]; then
+    SCRIPTS="$SCRIPT"
+fi
+SCRIPTS="${SCRIPTS:-smartmon.py}"
 OUTPUT_PATH="${OUTPUT_PATH:-/var/lib/node_exporter}"
 INTERVAL="${INTERVAL:-300}"
 
-PYTHON_SCRIPTS=("${SCRIPTS_PATH}"/*.py)
-if [ ${#PYTHON_SCRIPTS[@]} -eq 0 ]; then
-    echo "No Python scripts found in ${SCRIPTS_PATH}. Exiting 1"
-    exit 1
-fi
+IFS=',' read -ra SCRIPT_LIST <<< "$SCRIPTS"
 
-echo "Starting Python scripts loop ..."
+for SCRIPT in "${SCRIPT_LIST[@]}"; do
+    if [ ! -f "/scripts/${SCRIPT}" ]; then
+        echo "Script ${SCRIPT} doesn't exist. Exiting 1"
+        exit 1
+    fi
+    OUTPUT_FILENAME="${SCRIPT%.*}"
+    echo "Prepared to run ${SCRIPT} -> ${OUTPUT_PATH}/${OUTPUT_FILENAME}.prom"
+done
+
+echo "Starting scripts loop ..."
 while true; do
-    for script in "${PYTHON_SCRIPTS[@]}"; do
-        script_name=$(basename "$script" .py)
-        "$script" "${@}" | sponge "${OUTPUT_PATH}/${script_name}.prom"
+    for SCRIPT in "${SCRIPT_LIST[@]}"; do
+        OUTPUT_FILENAME="${SCRIPT%.*}"
+        "/scripts/${SCRIPT}" "$@" | sponge "${OUTPUT_PATH}/${OUTPUT_FILENAME}.prom"
     done
     sleep "${INTERVAL}"
 done
